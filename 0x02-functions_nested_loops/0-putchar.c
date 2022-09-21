bb4f1a7fc396c51fd572833e848edd8d9f489e39
lilydinka <76422926+lilydinka@users.noreply.github.com>
2022-09-21 14:37:12 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,14 @@
 
 int main(void)
 {
-	char str = "_putchar";
+	int n = 0;
+	char str_hbt[8] = "_putchar";
+
+	while (n < 8)
+	{
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
 	_putchar('\n');
 	return (0);
 }
